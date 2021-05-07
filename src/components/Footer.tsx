6960132176005352c4a1f1207a20007a4685bf8c
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-05-07 11:35:42 -0300

    fix: apps menu margin

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from '../assets/styles/components/footer.module.scss';
-
-function Footer() {
-  return <div className={styles.footerContainer}></div>;
-}
-
-export default Footer;
