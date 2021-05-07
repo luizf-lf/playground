022283dc3e620c3f8734f55b7cde205761be0c21
Luiz Fernando <luizfernando_lf@hotmail.com.br>
2021-05-07 17:09:29 -0300

    feat: Clicker component finished

--- src/pages/Clicker.tsx
+++ src/pages/Clicker.tsx
@@ -1,16 +1,41 @@
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import React from 'react';
+
+import styles from '../assets/styles/pages/clicker.module.scss';
 import globalContainerVariants from 'src/utils/globalContainerVariants';
+import { FaMousePointer } from 'react-icons/fa';
+import PageInfoCard from '../components/PageInfoCard';
 
 function Clicker() {
+  const [totalClicks, setTotalClicks] = useState(
+    Number(localStorage.getItem('pgClickerTotal'))
+  );
+
+  function handleClicks() {
+    setTotalClicks(totalClicks + 1);
+    localStorage.setItem('pgClickerTotal', String(totalClicks + 1));
+  }
+
   return (
     <motion.div
       variants={globalContainerVariants}
       initial="hidden"
       animate="visible"
       exit="exit"
+      className={styles.clickerContainer}
     >
-      Clicker
+      <span className={styles.clickTitle}>Total de cliques</span>
+      <span className={styles.clickIndicator}>{totalClicks}</span>
+      <button className={styles.clickButton} onClick={() => handleClicks()}>
+        <FaMousePointer />
+        Clicar +1 vez
+      </button>
+      <PageInfoCard>
+        Esta página foi criada para exercitar os conceitos de <i>state</i> e{' '}
+        <i>local storage</i>. Cada vez que o botão acima é clicado, o numero de
+        clicks é atualizado e seu valor é salvo no local storage do navegador,
+        ou seja, mesmo se a página for atualizada, o valor continuará salvo.
+      </PageInfoCard>
     </motion.div>
   );
 }
